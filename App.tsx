1225beef671307667dcec34392234aa598d040b6
Daniel Price <danieljamieprice@gmail.com>
2023-09-03 19:21:25 +0900

    Remove unnecessary code

--- App.tsx
+++ App.tsx
@@ -6,13 +6,12 @@ import { supabase } from "./supabaseService";
 import { Session } from "@supabase/supabase-js";
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session)
-      console.log(session)
-    })
+    });
 
     supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session)
@@ -21,7 +20,9 @@ export default function App() {
 
   return (
     <View>
-      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+      {session && session.user ? <Account key={session.user.id} session={session} /> 
+      : 
+    <Auth />}
     </View>
   )
 }
