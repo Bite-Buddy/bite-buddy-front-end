31635eddf55d248d307bb560e184c107f62256dd
Daniel Price <danieljamieprice@gmail.com>
2023-09-05 03:20:35 +0900

    Add development urls

---
+++ developmentUrls.ts
@@ -0,0 +1,7 @@
+// this is the ip address of the park and my home.
+// whilst we are on development, supabase will redirect users back to the app
+// after google sign in using this address
+export const devUrls = {
+  parkUrl: "exp://192.168.10.108:8081/",
+  danUrl: "exp://192.168.1.226:8081/",
+}
