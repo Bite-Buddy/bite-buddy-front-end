d4df12f497897a8545ae55d0b872198a7de67951
Emi <emi.hokama.eckstein@gmail.com>
2023-09-05 20:57:39 +0900

    Add 'AddFood' screen

---
+++ screens/AddFood.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, ScrollView, TextInput, Button } from 'react-native';
+import FoodInput from '../forms/FoodInput'
+
+
+export default function AddFood() {
+    type Items = {
+        name: string,
+        boughtOn: Date
+    }[]
+    const today = new Date();
+    const [itemNum, setItemNum] = useState<number>(1);
+    const [items, setItems] = useState<Items>([{ name: "", boughtOn: today }]);
+
+    return (
+        <ScrollView style={styles.container}>
+            <Text style={styles.headline}>Add New Item</Text>
+            {items.map((item, index) => { return <FoodInput key={index} item={item.name} boughtOn={item.boughtOn} /> })}
+            <Button
+                onPress={() => { setItemNum(itemNum + 1) }}
+                title={"+"} />
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 40,
+        padding: 12,
+    },
+    headline: {
+        margin: 10,
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    /**Copied below from the other component, not sure the intention */
+    // mt20: {
+    //   marginTop: 20,
+    // },
+})
