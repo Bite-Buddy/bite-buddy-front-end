165008f991b4e5366457bc00299cd73ba53ca9a9
izabelakucharska <karwowskaizabela0@gmail.com>
2023-09-05 17:11:07 +0900

    Change buttons to pressable components and styling of the navbar

--- header/Header.tsx
+++ header/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
+
 
 export default function Header() {
   const navigation = useNavigation();
@@ -9,12 +10,21 @@ export default function Header() {
   }
   return (
     <View style={styles.header}>
-      <Button title="Kitchen" color='#EFCA46' onPress={() => navigate('Kitchen')} />
-      <Button title="List" color='#EFCA46' onPress={() => navigate('List')} />
-      <Button title="Profile" color='#EFCA46' onPress={() => navigate('Profile')} />
+      <Pressable style={styles.button} title="Kitchen" onPress={() => navigate('Kitchen')}>
+      <Text style={styles.text}>Kitchen</Text>
+      </Pressable>
+      <Pressable style={styles.button} title="List" onPress={() => navigate('List')}>
+      <Text style={styles.text} Icon={{ type: 'font-awesome', name: 'envelope' }}>List</Text>
+      </Pressable>
+      <Pressable style={styles.button} title="Profile" onPress={() => navigate('Profile')}>
+      <Text style={styles.text}>Profile</Text>
+      </Pressable>
     </View>
   )
 }
+
+{/* <Button title="List" color='#EFCA46' onPress={() => navigate('List')} />
+<Button title="Profile" color='#EFCA46' onPress={() => navigate('Profile')} /> */}
 
 const styles = StyleSheet.create({
   header: {
@@ -25,12 +35,17 @@ const styles = StyleSheet.create({
    justifyContent: 'space-between',
    backgroundColor: '#EFCA46',
   },
-  headerText: {
+  text: {
     fontWeight: 'bold',
     fontSize: 20,
     color: '#333',
+    marginLeft: 10,
+    marginRight: 10,
+    
   },
   button: {
     color: 'black',
+    
+
   }
 })
