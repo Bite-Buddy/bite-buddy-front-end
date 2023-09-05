453d2c87b68901fdedb26e0abe472e81a3454da6
Emi <emi.hokama.eckstein@gmail.com>
2023-09-05 17:34:06 +0900

    Add 'AddFood' component as a form

---
+++ forms/AddFood.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, View, Text, ScrollView, TextInput } from 'react-native';
+
+const today = new Date()
+
+export default function AddFood() {
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.headline}>Add New Item</Text>
+      <View>
+        <Text style={styles.verticallySpaced}>Name</Text>
+        <TextInput
+          style={styles.userInput}
+          defaultValue="You can type in me"
+        />
+        <Text style={styles.verticallySpaced}>Bought on</Text>
+        <TextInput
+          style={styles.userInput}
+          defaultValue={today.toISOString()}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 12,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: 'stretch',
+  },
+  userInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  headline: {
+    margin: 5,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  /**Copied below from the other component, not sure the intention */
+  // mt20: {
+  //   marginTop: 20,
+  // },
+})
