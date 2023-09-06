31ebc6229eb7de5aa70f0ee1146783ada6fab42c
Daniel Price <danieljamieprice@gmail.com>
2023-09-06 19:45:03 +0900

    Fix network issues, Fix createuser,getbysupabaseid

--- fetchRequests.ts
+++ fetchRequests.ts
@@ -3,34 +3,36 @@
 const DOMAIN = process.env.DOMAIN;
 
 // use http and ip address instead of localhost
-// const domain = "http://192.168.10.108:8000"; //park local server
+// const DOMAIN = "http://192.168.10.108:8080"; //park local server
 
 export async function createUser(supabase_id: string, email: string):Promise<Response> {
-  const response = await fetch(`${DOMAIN}/users`, {
-    method: "POST",
-    body: JSON.stringify({
-      supabase_id: supabase_id,
-      email: email,
-    }),
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-    },
-  });
-  return response.json();
+  try {
+    const response = await fetch(`${DOMAIN}/users`, {
+      method: "POST",
+      body: JSON.stringify({
+        supabase_id: supabase_id,
+        email: email,
+      }),
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+  }
+  catch(error) {
+    throw error;
+  }
 }
 
 export async function getUsers():Promise<Response> {
   try {
     const response = await fetch(`${DOMAIN}/users`, {
       method: "GET",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      }
     });
-    const resolved = await response.json();
-    return resolved;
+    return response.json();
   }
   catch(error) {
     console.error(error);
@@ -39,10 +41,16 @@ export async function getUsers():Promise<Response> {
 }
 
 export async function getBySupabaseID(supabase_id: string):Promise<Response> {
-  const response = await fetch(`${DOMAIN}/users/:${supabase_id}`, {
-    method: "GET",
-  });
-  return response.json();
+  try {
+    const response = await fetch(`${DOMAIN}/users/supabase/${supabase_id}`, {
+      method: "GET",
+    });
+    return response.json();
+  }
+  catch(error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 async function createFood() {
