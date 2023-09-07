151d04b27e778519a0df8b5416597d62e3b1b922
izabelakucharska <karwowskaizabela0@gmail.com>
2023-09-07 20:02:33 +0900

    Test global state in AddKitchen component

--- screens/AddKitchen.tsx
+++ screens/AddKitchen.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, ScrollView, TextInput, Button } from 'react-native';
 import { Input } from "react-native-elements";
+import { useStateValue } from '../store/State'
 
 
 export default function AddKitchen() {
   const [name, setName] = useState('')
   const [loading, setLoading] = useState(false)
+  const [{ theme }, dispatch] = useStateValue();
+
+  function changeState() {
+    dispatch({
+      type: 'changeTheme',
+      newTheme: { primary: 'blue'}
+    })
+  }
 
   async function submitKitchen() {
     setLoading(true)
@@ -22,6 +31,8 @@ export default function AddKitchen() {
               autoCapitalize={'none'}
             />
             <Button title="Submit" disabled={loading} onPress={() => submitKitchen()} />
+            <Button title="State Test" onPress={() => changeState()} />
+            <Text style={styles.headline}>{theme.primary}</Text>
         </ScrollView>
     );
 }
