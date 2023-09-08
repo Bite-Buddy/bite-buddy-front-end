7638950430417542db0d3860bead088668abab52
Emi <emi.hokama.eckstein@gmail.com>
2023-09-09 00:13:07 +0900

    Refactor useContext state handling

--- store/State.tsx
+++ store/State.tsx
@@ -1,8 +1,20 @@
-import React, {createContext, useContext, useReducer} from 'react';
-export const StateContext = createContext();
-export const StateProvider = ({reducer, initialState, children}) =>(
-  <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {children}
-  </StateContext.Provider>
-);
-export const useStateValue = () => useContext(StateContext);
+import { Dispatch, FC, ReactNode, Reducer, createContext, useContext, useReducer } from "react";
+import { reducer, IMutualState, TAction } from "./reducer";
+
+const StateContext = createContext<[IMutualState, Dispatch<TAction>] | undefined>(undefined);
+
+interface IStateProviderProps {
+  reducer: Reducer<IMutualState, TAction>;
+  initialState: IMutualState;
+  children: ReactNode;
+}
+
+const StateProvider: FC<IStateProviderProps> = ({ reducer, initialState, children }) => {
+  return (
+    < StateContext.Provider value={useReducer(reducer, initialState)} >
+      {children}
+    </StateContext.Provider >);
+}
+const useStateValue = () => useContext(StateContext);
+
+export { StateContext, IStateProviderProps, StateProvider, useStateValue }
