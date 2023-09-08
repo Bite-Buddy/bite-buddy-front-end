0ba20392b38f0bffcc7c732df75c77d89fe22358
Emi <emi.hokama.eckstein@gmail.com>
2023-09-09 00:27:38 +0900

    Sort import of screens in one line

--- App.tsx
+++ App.tsx
@@ -4,14 +4,8 @@ import { supabase } from "./supabaseService";
 import { Session } from "@supabase/supabase-js";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Auth from "./screens/Auth";
-import Account from "./screens/Account";
-import Kitchen from "./screens/Kitchen";
-import Profile from "./screens/Profile";
-import List from "./screens/List";
+import { Auth, Account, Kitchen, Profile, List, AddFood, AddKitchen } from "./screens/screens";
 import Header from "./header/Header";
-import AddFood from "./screens/AddFood";
-import AddKitchen from './screens/AddKitchen'
 import { createUser, getBySupabaseID } from "./fetchRequests";
 import { StateProvider } from './store/State';
 import { reducer } from './store/reducer'
@@ -81,55 +75,55 @@ export default function App() {
   return (
     <StateProvider initialState={initialState} reducer={reducer}>
 
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={session ? "Account" : "Auth"}>
-        <Stack.Screen name="Auth" component={Auth} />
-        <Stack.Screen name="Account" component={Account} options={({ navigation }) => ({
-          headerTitle: () => <Header />,
-          headerStyle: {
-            backgroundColor: '#EFCA46',
-          },
-          
-        })} />
-        <Stack.Screen name="Kitchen" component={Kitchen} options={({ navigation }) => ({
-          headerTitle: () => <Header />,
-          headerStyle: {
-            backgroundColor: '#EFCA46',
-          },
-          
-        })}/>
-        <Stack.Screen name="Profile" component={Profile} options={({ navigation }) => ({
-          headerTitle: () => <Header />,
-          headerStyle: {
-            backgroundColor: '#EFCA46',
-          },
-          
-        })}/>
-        <Stack.Screen name="List" component={List} options={({ navigation }) => ({
-          headerTitle: () => <Header />,
-          headerStyle: {
-            backgroundColor: '#EFCA46',
-          },
-          
-        })}/>
-        <Stack.Screen name="AddFood" component={AddFood} options={({ navigation }) => ({
-          headerTitle: () => <Header />,
-          headerStyle: {
-            backgroundColor: '#EFCA46',
-          },
-          
-        })}/>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={session ? "Account" : "Auth"}>
+          <Stack.Screen name="Auth" component={Auth} />
+          <Stack.Screen name="Account" component={Account} options={({ navigation }) => ({
+            headerTitle: () => <Header />,
+            headerStyle: {
+              backgroundColor: '#EFCA46',
+            },
 
-        <Stack.Screen name="AddKitchen" component={AddKitchen} options={({ navigation }) => ({
-          headerTitle: () => <Header />,
-          headerStyle: {
-            backgroundColor: '#EFCA46',
-          },
-          
-        })}/>
-        {/* Add more screens as needed */}
-      </Stack.Navigator>
-    </NavigationContainer>
+          })} />
+          <Stack.Screen name="Kitchen" component={Kitchen} options={({ navigation }) => ({
+            headerTitle: () => <Header />,
+            headerStyle: {
+              backgroundColor: '#EFCA46',
+            },
+
+          })} />
+          <Stack.Screen name="Profile" component={Profile} options={({ navigation }) => ({
+            headerTitle: () => <Header />,
+            headerStyle: {
+              backgroundColor: '#EFCA46',
+            },
+
+          })} />
+          <Stack.Screen name="List" component={List} options={({ navigation }) => ({
+            headerTitle: () => <Header />,
+            headerStyle: {
+              backgroundColor: '#EFCA46',
+            },
+
+          })} />
+          <Stack.Screen name="AddFood" component={AddFood} options={({ navigation }) => ({
+            headerTitle: () => <Header />,
+            headerStyle: {
+              backgroundColor: '#EFCA46',
+            },
+
+          })} />
+
+          <Stack.Screen name="AddKitchen" component={AddKitchen} options={({ navigation }) => ({
+            headerTitle: () => <Header />,
+            headerStyle: {
+              backgroundColor: '#EFCA46',
+            },
+
+          })} />
+          {/* Add more screens as needed */}
+        </Stack.Navigator>
+      </NavigationContainer>
     </StateProvider>
   )
 }
