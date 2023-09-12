642d3fb81e3f74e160bdbb5da1a1214a631a899f
Daniel Price <danieljamieprice@gmail.com>
2023-09-12 23:03:59 +0900

    Make delete button

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -2,11 +2,19 @@ import { Pressable, StyleSheet, View } from "react-native";
 import { Text } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 import { supabase } from "../supabaseService";
+import { deleteUserFromDatabase } from "../utilities/fetchRequests";
+import { useAtom } from "jotai";
+import { userAtom } from "../utilities/store/atoms";
 
 export default function Profile() {
+const [user, setUser] = useAtom(userAtom);
 const navigation = useNavigation();
 
 async function signOutHandler() {
+  navigation.navigate("Auth");
+}
+async function deleteHandler() {
+  await deleteUserFromDatabase(user.id, user.supabase_id);
   navigation.navigate("Auth");
 }
   return (
@@ -15,6 +23,9 @@ async function signOutHandler() {
         <Text>Profile</Text>
         <Pressable onPressIn={() => signOutHandler()}>
           <Text>Sign Out</Text>
+        </Pressable>
+        <Pressable onPressIn={() => deleteHandler()}>
+          <Text>Delete Account</Text>
         </Pressable>
       </View>
     </View>
