198efd0d348e9e46a7539895a345711f779841d4
izabelakucharska <karwowskaizabela0@gmail.com>
2023-09-13 01:36:27 +0900

    Reset auth component

--- screens/Auth.tsx
+++ screens/Auth.tsx
@@ -1,115 +1,33 @@
-import React, { useState } from "react";
-import { Alert, StyleSheet, View, Pressable } from "react-native";
-import { Text, Button, Input } from "react-native-elements";
-import { supabase } from "../supabaseService";
-import { Provider } from "@supabase/supabase-js";
-import * as WebBrowser from "expo-web-browser";
-import { useNavigation } from "@react-navigation/native";
-// import * as Linking from "expo-linking";
-import { devUrls } from "../utilities/developmentUrls";
-import { createUser, getBySupabaseID } from "../utilities/fetchRequests";
-import { userAtom, kitchensAtom } from "../utilities/store/atoms";
-import { useAtom } from "jotai";
+import React, { useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
+import { supabase } from '../supabaseService';
+import { Button, Input } from 'react-native-elements';
 
 export default function Auth() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [user, setUser] = useAtom(userAtom);
-  const [kitchens, setKitchens] = useAtom(kitchensAtom);
-  const navigation = useNavigation();
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
 
-  // async function signInWithEmail() {
-  //   setLoading(true);
-  //   const { error } = await supabase.auth.signInWithPassword({
-  //     email: email,
-  //     password: password,
-  //   })
+  async function signInWithEmail() {
+    setLoading(true)
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    })
 
-  //   if (error) Alert.alert(error.message)
+    if (error) Alert.alert(error.message)
+    setLoading(false)
+  }
 
-  //   setLoading(false);
-  //   console.log("USER ON SIGN IN, SUPABASE ID: ", (await supabase.auth.getSession()).data.session?.user.id);
-  // }
+  async function signUpWithEmail() {
+    setLoading(true)
+    const { error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    })
 
-  // async function signUpWithEmail() {
-  //   setLoading(true);
-  //   const { error } = await supabase.auth.signUp({
-  //     email: email,
-  //     password: password,
-  //   })
-
-  //   if (error) Alert.alert(error.message)
-  //   setLoading(false);
-  // }
-
-  //this long ass mess is a workaround for supabase.auth.signInWithOauth
-  // which doesn't seem to want to store the session tokens etc
-  async function logInWithThirdParty(provider: Provider) {
-    console.log('fuck')
-    setLoading(true);
-    // try {
-    //   const supabase_url = "https://qlpmqnbgyofvhqyhxvhi.supabase.co";
-    //   const redirectUri = devUrls.izaUrl; 
-    //   const response = await WebBrowser.openAuthSessionAsync(
-    //     `${supabase_url}/auth/v1/authorize?provider=${provider}&redirect_to=${redirectUri}`,
-    //     redirectUri
-    //   );
-
-    //   if (response.type === "success") {
-    //     const url = response.url;
-    //     const params = url.split("#")[1];
-    //     const accessToken = params.split("&")[0].split("=")[1];
-    //     const refreshToken = params.split("&")[2].split("=")[1];
-
-    //     const { data, error } = await supabase.auth.setSession({
-    //       access_token: accessToken,
-    //       refresh_token: refreshToken,
-    //     });
-    //     if (error) Alert.alert(error.message);
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    // } finally {
-    //   WebBrowser.maybeCompleteAuthSession();
-    // }
-    // const sesh = await supabase.auth.getSession();
-    if (true) {
-      // const supabaseId = sesh.data.session?.user.id;
-      console.log('wtf')
-      const dbData = await getBySupabaseID("5777eeba-1c47-48e6-aae8-c45a44fhd54");
-      console.log(dbData)
-      // if (dbData.failed) {
-      //   // console.log("Supabase ID", sesh.data.session?.user.id);
-      //   console.log("DATBASE DATA", dbData);
-      //   try {
-      //     const newUser = await createUser(supabaseId, sesh.data.session?.user.email);
-      //     if (newUser) {
-      //       setUser(newUser);
-      //       setKitchens(newUser.kitchens);
-      //       console.log("THE NEW USER", newUser);
-      //     }
-      //   }
-      //   catch (error) {
-      //     console.error(error);
-      //     throw error;
-      //   }
-      // }
-      // else {
-      //   try {
-          setUser(dbData);
-          console.log(dbData)
-          setKitchens(dbData.kitchens)
-          console.log(dbData.kitchens)
-    //     }
-    //     catch (error) {
-    //       console.log(error);
-    //       throw error;
-        }
-    //   }
-    // }
-    user.kitchens.length > 0 ? navigation.navigate("Kitchen") : navigation.navigate("Account");
-    setLoading(false);
+    if (error) Alert.alert(error.message)
+    setLoading(false)
   }
 
   return (
@@ -136,13 +54,10 @@ export default function Auth() {
         />
       </View>
       <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Pressable disabled={loading} style={styles.button} onPress={() => signInWithEmail()} ><Text style={styles.buttonText}>Sign in</Text></Pressable>
+        <Button title="Sign in" disabled={loading} onPress={() => signInWithEmail()} />
       </View>
       <View style={styles.verticallySpaced}>
-        <Pressable disabled={loading} style={styles.button} onPress={() => signUpWithEmail()} ><Text style={styles.buttonText}>Sign up</Text></Pressable>
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Button title="Google" disabled={loading} onPress={() => logInWithThirdParty("google")} />
+        <Button title="Sign up" disabled={loading} onPress={() => signUpWithEmail()} />
       </View>
     </View>
   )
@@ -157,17 +72,6 @@ const styles = StyleSheet.create({
     paddingTop: 4,
     paddingBottom: 4,
     alignSelf: 'stretch',
-  },
-  button: {
-    backgroundColor: '#EFCA46',
-    height: 40,
-    borderRadius: 4,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonText: {
-    fontWeight: "bold"
   },
   mt20: {
     marginTop: 20,
