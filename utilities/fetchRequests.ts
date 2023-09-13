9784ccf8c128ef8c9a44253e677a03535c48e2bf
Kingshuk Roy <kingshukroy98@gmail.com>
2023-09-13 19:57:08 +0900

    Update domain to backend url ENV variable (will be Heroku link)

--- utilities/fetchRequests.ts
+++ utilities/fetchRequests.ts
@@ -1,9 +1,9 @@
 //configure your .env.local file with the server domain link
 //REMOVE THE TRAILING SLASH (/) FROM THE URL IN YOUR .env.local FILE
-// const DOMAIN = process.env.DOMAIN;
+const DOMAIN = process.env.BACKEND_URL;
 
 // use http and ip address instead of localhost
-const DOMAIN = "http://192.168.1.226:8000"; //park local server
+//const DOMAIN = "http://192.168.1.226:8000"; //park local server
 import { supabase } from "../supabaseService";
 import { IKitchen, IUser, IFood, IFoodRequest } from "./interfaces";
 // const DOMAIN = "http://localhost:8080";
