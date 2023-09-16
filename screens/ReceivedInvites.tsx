6778a650c6425ea8ba580d3e7ceb4044dad94537
Kingshuk Roy <kingshukroy98@gmail.com>
2023-09-16 21:13:00 +0900

    Initial received invites page template

---
+++ screens/ReceivedInvites.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, View } from "react-native";
+import { Text } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+
+export default function receivedInvites() {
+    return (
+        <View style={styles.container}>
+          <View style={styles.verticallySpaced}>
+            <Text>Kitchen Members</Text>
+          </View>
+        </View>
+      )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      marginTop: 40,
+      padding: 12,
+    },
+    verticallySpaced: {
+      paddingTop: 4,
+      paddingBottom: 4,
+      alignSelf: 'stretch',
+    },
+    mt20: {
+      marginTop: 20,
+    },
+  })
