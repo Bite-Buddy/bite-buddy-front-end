da8bbb81a75046fe67fa3522b8e5e44aded89fcb
Kingshuk Roy <kingshukroy98@gmail.com>
2023-09-17 16:58:53 +0900

    Develop and refactor receivedInvites screen

--- screens/ReceivedInvites.tsx
+++ screens/ReceivedInvites.tsx
@@ -1,22 +1,33 @@
-import { Modal, Pressable, StyleSheet, View } from "react-native";
+import { Modal, Pressable, StyleSheet, ScrollView, View } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
-import { invitesAtom } from "../utilities/store/atoms";
+import { invitesAtom, currentInviteAtom } from "../utilities/store/atoms";
 import { useAtom } from "jotai";
 import { useNavigation } from "@react-navigation/native";
-import { getByDatabaseID, getKitchenByID } from "../utilities/fetchRequests";
+import { getByDatabaseID, getInviteById, getKitchenByID } from "../utilities/fetchRequests";
 import { useState, useEffect } from "react";
+import { IInvite, IReceivedInvite } from "../utilities/interfaces";
 
 export default function ReceivedInvites() {
     const [invites, setInvites] = useAtom(invitesAtom)
-    const [inviteNames, setInviteNames] = useState<string[]>([]);
+    const [inviteNames, setInviteNames] = useState<IReceivedInvite[]>([]);
+    const [currentInvite, setCurrentInvite] = useAtom(currentInviteAtom);
+    const navigation = useNavigation();
 
+    useEffect(() => {
+      if (invites) {
+        fetchFoodNames()
+      }
+    }, [invites])
     const fetchFoodNames = async () => {
       try {
         if (invites) {
             let kitchenIds = invites.map((invite => invite.kitchen_id))
             let kitchenInfo = kitchenIds.map((id => getKitchenByID(id)))
             let kitchenNames = await Promise.all(kitchenInfo.map(async (kitchen) => {
-              return (await kitchen).name
+              let receivedInvite: IReceivedInvite = {name: '', id: 0};
+              receivedInvite.name = (await kitchen).name;
+              receivedInvite.id = (await kitchen).id;
+              return receivedInvite;
             }));
             setInviteNames(kitchenNames)
           }
@@ -26,89 +37,108 @@ export default function ReceivedInvites() {
         throw error;
       }
     }
+    function selectInvite(invite: IInvite) {
+      navigation.navigate("InviteResponse")
+      setCurrentInvite(invite);
+    }
 
 
     return (
-        <View style={styles.container}>
-          <View style={styles.verticallySpaced}>
-            <Text>Received Invites</Text>
-          </View>
+      <View style={styles.container}>
+        <View style={styles.verticallySpaced}>
+          <Text style={styles.heading}>Received Invites</Text>
+          <ScrollView>
+            <View>
+              <View>
+                {inviteNames.map(invite => {
+                return (
+                  <Pressable key={`selectableInvite${invite.id}`} style={styles.list} onPress={async () => selectInvite(await getInviteById(invite.id))}>
+                    <Text style={styles.kitchen}>
+                      You've been invited to {invite.name}
+                    </Text>
+                  </Pressable>
+                )})}
+              </View>
+            </View>
+  
+          </ScrollView>
         </View>
-      )
+      </View>
+    )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'space-between',
-      backgroundColor: '#F8E8AF',
-      padding: 20,
-      margin: 0,
-  
-    },
-    verticallySpaced: {
-      flex: 1,
-      borderWidth: 0,
-      borderRadius: 20,
-      marginBottom: 20,
-    },
-    mt20: {
-      marginTop: 20,
-    },
-    kitchen: {
-      fontSize: 18,
-      fontWeight: "bold"
-    },
-    heading: {
-      fontSize: 20,
-      marginTop: 10,
-      marginBottom: 0,
-      flex: 1,
-      fontWeight: 'bold',
-      color: 'black',
-      textAlign: 'center',
-    },
-    name: {
-      padding: 2,
-      fontSize: 15,
-      fontWeight: "bold",
-      marginTop: 5,
-      marginLeft: 10,
-    },
-    date: {
-      padding: 0,
-      fontSize: 15,
-      marginTop: 10,
-      marginLeft: 30,
-      marginRight: 10
-    },
-    list: {
-      flexDirection: "row",
-      backgroundColor: '#EFCA46',
-      borderWidth: 0,
-      justifyContent: 'center',
-      alignItems: "center",
-      marginTop: 5,
-      marginLeft: 10,
-      marginRight: 10,
-      height: 60,
-      borderTopLeftRadius: 7,
-      borderTopRightRadius: 7,
-      borderBottomLeftRadius: 7,
-      borderBottomRightRadius: 7,
-    },
-    button: {
-      display: 'flex',
-      alignItems: 'center',
-      alignSelf: 'center',
-      backgroundColor: '#4dd377',
-      width: 50,
-      height: 50,
-      justifyContent: "center",
-      borderRadius: 50,
-      marginTop: 20
-    },
-    icon: {
-      fontSize: 36
-    }
-  })
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#F8E8AF',
+    padding: 20,
+    margin: 0,
+
+  },
+  verticallySpaced: {
+    flex: 1,
+    borderWidth: 0,
+    borderRadius: 20,
+    marginBottom: 20,
+  },
+  mt20: {
+    marginTop: 20,
+  },
+  kitchen: {
+    fontSize: 18,
+    fontWeight: "bold"
+  },
+  heading: {
+    fontSize: 20,
+    marginTop: 10,
+    marginBottom: 0,
+    flex: 1,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+  },
+  name: {
+    padding: 2,
+    fontSize: 15,
+    fontWeight: "bold",
+    marginTop: 5,
+    marginLeft: 10,
+  },
+  date: {
+    padding: 0,
+    fontSize: 15,
+    marginTop: 10,
+    marginLeft: 30,
+    marginRight: 10
+  },
+  list: {
+    flexDirection: "row",
+    backgroundColor: '#EFCA46',
+    borderWidth: 0,
+    justifyContent: 'center',
+    alignItems: "center",
+    marginTop: 5,
+    marginLeft: 10,
+    marginRight: 10,
+    height: 60,
+    borderTopLeftRadius: 7,
+    borderTopRightRadius: 7,
+    borderBottomLeftRadius: 7,
+    borderBottomRightRadius: 7,
+  },
+  button: {
+    display: 'flex',
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#4dd377',
+    width: 50,
+    height: 50,
+    justifyContent: "center",
+    borderRadius: 50,
+    marginTop: 20
+  },
+  icon: {
+    fontSize: 36
+  }
+})
