ab09c985eb9c7db74f6b09582f64b1ae1b7c7e16
Daniel Price <danieljamieprice@gmail.com>
2023-09-18 13:16:04 +0900

    Remove unused dependencies

--- App.tsx
+++ App.tsx
@@ -1,27 +1,14 @@
-import { supabase } from "./supabaseService";
-import { Session } from "@supabase/supabase-js";
-import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Header from "./header/Header";
 import { Auth, Account, Profile, List, EditFood, AddFood, AddKitchen, BarcodeScan, InviteResponse, ReceivedInvites } from "./screens/screens";
 import Drawer from "./screens/Drawer";
-import { createUser, getBySupabaseID, getUsers } from "./utilities/fetchRequests";
-import { IUser } from "./utilities/interfaces";
-import { userAtom, kitchensAtom } from './utilities/store/atoms'
-import { useAtom } from 'jotai'
 
 const Stack = createStackNavigator();
 
-// const { error } = await supabase.auth.signOut() storing this for later
-
 export default function App() {
-  const [sessionChecked, setSessionChecked] = useState(false);
-  const [kitchens, setKitchens] = useAtom(kitchensAtom);
-  const [user, setUser] = useAtom(userAtom);
-  const [session, setSession] = useState<Session | null>(null);
- 
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Auth">
